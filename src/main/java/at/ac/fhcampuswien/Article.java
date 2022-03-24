02a859b0795bab23a04434006d29af4ede365a1b
gmlynar <gabriel.mlynar@stud.fh-campuswien.ac.at>
2022-03-24 23:39:27 +0100

    Added Article.java

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -1,4 +1,24 @@
 package at.ac.fhcampuswien;
 
 public class Article {
+    private String author;
+    private String title;
+
+    public Article(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor(String author){
+        return author;
+    }
+
+    public String getTitle(String title){
+        return title;
+    }
+
+    @Override
+    public String toString(){
+        return "Title: " + title + System.lineSeparator() + "Author: " + author;
+    }
 }
