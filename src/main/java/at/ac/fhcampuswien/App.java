ac77c99305a28d7d1f8b3730afdf5e7f9167d284
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2022-03-27 23:44:38 +0200

    ...

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,7 +1,24 @@
 package at.ac.fhcampuswien;
 
-public class App {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class App extends Application {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("NewsApp");
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
