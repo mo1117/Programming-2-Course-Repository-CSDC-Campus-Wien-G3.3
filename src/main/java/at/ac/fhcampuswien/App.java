e021226555929db8ea870ab577dd7415e422ffa2
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2022-04-24 15:30:38 +0200

    See the request example in App - main() Finish NewsApi by Tuesday please Other classes will be done by us

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,13 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import at.ac.fhcampuswien.NewsApi;
 
+import java.io.IOException;
 
 public class App extends Application {
     public static void main(String[] args) {
          //Menu menu = new Menu();
          //menu.start();
-         launch();
+         //launch();
+
+        NewsApi api = new NewsApi();
+        try {
+            System.out.println(api.request("https://newsapi.org/v2/everything?q=putin&apiKey=2f62c3bf2ba84f97adb6eaf246dccced"));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/main/java/at/ac/fhcampuswien/NewsApi.java
+++ src/main/java/at/ac/fhcampuswien/NewsApi.java
@@ -1,10 +1,26 @@
 package at.ac.fhcampuswien;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 public class NewsApi {
     //key: 2f62c3bf2ba84f97adb6eaf246dccced
     //https://newsapi.org/v2/everything?q=bitcoin&apiKey=2f62c3bf2ba84f97adb6eaf246dccced
 
+    OkHttpClient client = new OkHttpClient();
+
+    public String request(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()){
+            return response.body().string();
+        }
+    }
 
 }
